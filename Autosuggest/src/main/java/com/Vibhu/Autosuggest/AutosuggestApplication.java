57fed0ac3f0d261c1016b5be58c0e2ddac3685a5
Vibhu Vikhyat <vibhuvikhyat18@gmail.com>
2026-07-17 15:06:33 +0530

    intitial second

---
+++ Autosuggest/src/main/java/com/Vibhu/Autosuggest/AutosuggestApplication.java
@@ -0,0 +1,13 @@
+package com.Vibhu.Autosuggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosuggestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Vibhu/Autosuggest/AutosuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vibhu.Autosuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
